25f519d1612e99c1b9808b3d07d6802844e79dc5
paulomachadodev <paulomachadodev@gmail.com>
2023-05-08 21:49:45 -0300

    first commit

---
+++ Projeto04/Interfaces/IEmpresaRepository.cs
@@ -0,0 +1,22 @@
+﻿using Projeto04.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto04.Interfaces
+{
+    public interface IEmpresaRepository : IBaseRepository<Empresa>
+    {
+        void Add(Empresa empresa);
+
+        void Update(Empresa empresa);
+
+        void Delete(Empresa empresa);
+
+        List<Empresa> GetAll();
+
+        Empresa? GetById(Guid id);
+    }
+}
---
+++ Projeto04/Interfaces/IFuncionarioRepository.cs
@@ -0,0 +1,22 @@
+﻿using Projeto04.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto04.Interfaces
+{
+    public interface IFuncionarioRepository : IBaseRepository<Funcionario>
+    {
+        void Add(Funcionario funcionario);
+
+        void Update(Funcionario funcionario);
+
+        void Delete(Funcionario funcionario);
+
+        List<Funcionario> GetAll();
+
+        Funcionario? GetById(Guid id);
+    }
+}
